9fcb795a9aa5b8a93505728532db5fc34a725e9d
Walid <walidvb@gmail.com>
2016-06-05 19:05:38 +0200

    adds filter

--- app/partners/PartnersList.js
+++ app/partners/PartnersList.js
@@ -6,12 +6,27 @@ import {
   StyleSheet,
   ListView,
   Text,
+  TextInput,
   View,
   Image,
   TouchableHighlight,
 } from 'react-native'
 var baseStyles = require('../styles')
 
+function filterPartnersBy(partners, q){
+    if(!q || !q.length){
+      return partners;
+    }
+    let selected = [];
+    const regExp = new RegExp(q, 'i');
+    for(let i = 0; i < partners.length; i++){
+      if(regExp.test(partners[i].name) || regExp.test(partners[i].category)){
+        console.log(regExp, partners[i].name, partners[i].category);
+        selected.push(partners[i])
+      }
+    }
+    return selected
+}
 
 class PartnersList extends Component{
   constructor(props) {
@@ -23,6 +38,7 @@ class PartnersList extends Component{
       ...this.props,
       dataSource: new ListView.DataSource({
         rowHasChanged: (row1, row2) => row1 !== row2,
+        sectionHeaderHasChanged: (s1, s2) => s1 !== s2
       }),
       loaded: this.props.loaded,
     }
@@ -31,45 +47,74 @@ class PartnersList extends Component{
   }
 
   componentDidMount() {
-    console.log('redoing this', this.props);
      this.setState({
        ...this.state,
        partners: this.props.partners,
        dataSource: this.state.dataSource.cloneWithRows(this.props.partners)
      })
   }
+  componentWillReceiveProps(props){
+    if(this.state.parnters !== props.partners){
+      var filteredPartners = filterPartnersBy(props.partners, this.state.query);
+      this.setState({
+        ...this.state,
+        partners: props.partners,
+        dataSource: this.state.dataSource.cloneWithRowsAndSections(this._partnersAsBlob(filteredPartners))
+      });
+    }
+  }
+
   render() {
-    // TODO: move this where it belongs
-    const dataSource = this.state.dataSource.cloneWithRows(this.props.partners)
-    if(!dataSource.getRowCount()){
-      return (
+    const List = !this.state.partners.length ? (
         <View style={baseStyles.container}>
           <Text>
             Loading partners...
           </Text>
         </View>
+      ) : (
+        <ListView
+          automaticallyAdjustContentInsets={true}
+          style={[styles.partnersList]}
+          dataSource={this.state.dataSource}
+          renderHeader={this.renderHeader.bind(this)}
+          renderRow={this.renderPartnerCell}
+          renderSectionHeader={this.renderSectionHeader}
+        />
+      )
+      return (
+        <View style={{flex:1}}>
+
+          {List}
+        </View>
       );
-    }
-    return (
-      <ListView
-        automaticallyAdjustContentInsets={true}
-        style={[styles.partnersList]}
-        dataSource={dataSource}
-        renderRow={this.renderPartnerCell}
-      />
-    );
+  }
+  filter(query){
+    const filteredPartners = filterPartnersBy(this.props.partners, query);
+    this.setState({
+      ...this.state,
+      query: query,
+      dataSource: this.state.dataSource.cloneWithRowsAndSections(this._partnersAsBlob(filteredPartners))
+    });
+
+  }
+  renderHeader(){
+    return(
+      <TextInput style={{height: 60, padding: 5,}} ref='search' onChangeText={this.filter.bind(this)} placeholder='Search'/>
+    )
   }
   renderSectionHeader(sectionData, sectionID){
     return (
-      <Text
-        style={{
-          color: 'black',
-          fontSize: 16,
-        }}>
-        {sectionID}
-      </Text>
+      <View style={{alignItems: 'center', padding: 10, borderBottomColor: '#111', borderBottomWidth: 1} }>
+        <Text
+          style={{
+            color: 'black',
+            fontSize: 16,
+          }}>
+          {sectionID}
+        </Text>
+        </View>
     )
-  } 
+  }
 
   renderPartnerCell(partner){
     const goToPartner = () => {
@@ -87,6 +132,19 @@ class PartnersList extends Component{
       </TouchableHighlight>
     )
   }
+
+  _partnersAsBlob(partners){
+    if(!partners.length){
+      return [];
+    }
+    var dataBlob = {};
+    for(let i = 0; i < partners.length; i++){
+      let partner = partners[i];
+      dataBlob[partner.category] = dataBlob[partner.category] || []
+      dataBlob[partner.category].push(partner)
+    }
+    return dataBlob;
+  }
 };
 
 const styles = StyleSheet.create({
@@ -98,7 +156,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#0074D9',
   },
   thumbnail: {
     width: 30,
