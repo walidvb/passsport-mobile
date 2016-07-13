ec29017cb279f9ff0cc607484e034f4b3cba1c67
Walid <walidvb@gmail.com>
2016-07-13 11:44:21 +0200

    adds categories

--- app/categories/categoriesReducer.js
+++ app/categories/categoriesReducer.js
@@ -1,9 +1,9 @@
 export default function categories(state = {}, action){
   switch(action.type){
     case 'PARTNERS_FETCHED':
-      console.log('asdsas', action);
-      let categories = ['yes', 'no']
-      return categories;
+      cats = []
+      action.partners.map((p) => cats.push(...p.categories))
+      return [...new Set(cats)];
     default:
       return state;
   }
