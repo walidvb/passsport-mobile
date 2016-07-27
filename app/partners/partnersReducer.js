986aa673bd71b459518cfe96722a0f54ec295bd6
Walid <walidvb@gmail.com>
2016-07-27 19:08:15 +0200

    sorts partners

--- app/partners/partnersReducer.js
+++ app/partners/partnersReducer.js
@@ -2,7 +2,19 @@ export default function partners(state = [], action){
   switch(action.type){
     case 'PARTNERS_FETCHED':
       if(action.status != 'failed'){
-        return action.partners
+        return action.partners.sort(function(a, b) {
+          const nameA = a.name.toUpperCase().trim(); // ignore upper and lowercase
+          const nameB = b.name.toUpperCase().trim(); // ignore upper and lowercase
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+
+          // names must be equal
+          return 0;
+        });
       }
     default:
       return state;
