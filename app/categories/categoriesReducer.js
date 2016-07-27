d6943374115979d27fbbe2c1848887fdad891750
Walid <walidvb@gmail.com>
2016-07-27 19:03:15 +0200

    sorts cats

--- app/categories/categoriesReducer.js
+++ app/categories/categoriesReducer.js
@@ -3,7 +3,7 @@ export default function categories(state = {}, action){
     case 'PARTNERS_FETCHED':
       cats = new Set()
       action.partners.map((p) => cats.add(...p.categories.map((c)=>c.name)))
-      cats = Array.from(cats).filter((c) => !c.children_count)
+      cats = Array.from(cats).filter((c) => !c.children_count).sort()
       return [...cats];
     default:
       return state;
