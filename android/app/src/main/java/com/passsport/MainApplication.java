6201faafc54d20ea0ec1ba668e1f9538499d3c61
Walid <walidvb@gmail.com>
2016-08-23 22:56:49 +0200

    first port to android

--- android/app/src/main/java/com/passsport/MainApplication.java
+++ android/app/src/main/java/com/passsport/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
 import com.airbnb.android.react.maps.MapsPackage;
+import com.smixx.fabric.FabricPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-              new MapsPackage()
+              new MapsPackage(),
+              new FabricPackage()
       );
     }
   };
